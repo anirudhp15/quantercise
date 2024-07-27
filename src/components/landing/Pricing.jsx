7cd0cd1a35ae941de4e8249ecd611641be6d71f8
Anirudh Pottammal <anipotts@anirudhs-mbp.lan>
2024-07-27 03:11:04 -0400

    pics folder structure update

--- src/components/landing/Pricing.jsx
+++ src/components/landing/Pricing.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { FaArrowRightLong } from "react-icons/fa6";
-import { Link as ScrollLink, scroller } from "react-scroll";
+import { Link as ScrollLink } from "react-scroll";
 import axios from "axios";
 
 const Pricing = () => {
@@ -26,154 +26,121 @@ const Pricing = () => {
   return (
     <div
       id="pricing"
-      className="relative min-h-screen p-16 my-auto text-gray-300 lg:p-32"
+      className="relative min-h-screen p-16 my-auto text-gray-300 bg-black lg:p-32"
     >
       <div className="mx-auto text-center">
-        <h2 className="relative py-8 text-4xl font-bold text-transparent z-2 md:pb-12 md:text-5xl bg-clip-text bg-gradient-to-r from-green-400 via-blue-500 to-purple-500">
+        <h2 className="relative py-8 text-4xl font-bold text-transparent z-2 md:pb-12 md:text-5xl bg-clip-text bg-gradient-to-r from-purple-400 to-blue-600">
           Pricing Plans
         </h2>
         <p className="relative pb-8 text-lg font-medium text-white z-2 sm:text-xl md:text-2xl">
           Choose the plan that best fits your needs.
         </p>
+        <p className="relative pb-8 text-lg font-light text-white z-2 sm:text-xl md:text-2xl">
+          Get notified when{" "}
+          <span className="font-medium text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-purple-500">
+            Quantercise Pro
+          </span>{" "}
+          goes live for unlimited access to all questions, detailed feedback,
+          unlimited hints, and exclusive resources.
+        </p>
       </div>
-      <div className="flex justify-center">
-        <div className="relative z-10 w-auto p-4 mt-8 transition-all duration-200 border-2 border-gray-500 md:p-10 bg-gray-950 rounded-3xl hover:border-gray-300">
-          <p className="py-2 text-lg font-light text-center sm:text-xl md:text-2xl">
-            Get notified when{" "}
-            <span className="relative font-semibold text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-purple-500">
-              Quantercise Pro
-            </span>{" "}
-            goes live for unlimited access to all questions, detailed feedback,
-            unlimited hints, and exclusive resources.
+      <div className="relative z-10 flex flex-col items-center justify-center lg:flex-row lg:justify-around lg:gap-8">
+        <div className="w-full max-w-sm p-6 mt-8 transition-all duration-200 border-2 border-gray-500 bg-gray-950 rounded-xl hover:border-gray-300">
+          <h3 className="text-xl font-medium text-green-400 sm:text-md">
+            Starter
+          </h3>
+          <p className="mt-2 text-3xl font-bold text-gray-300 xl:text-4xl">
+            <span className="text-gray-300">$0</span>
+            <span className="ml-2 text-sm text-gray-300">(free)</span>
           </p>
-          <div className="grid gap-8 px-4 mx-auto mt-4 text-center lg:grid-cols-3">
-            <div className="relative group">
-              <div className="absolute inset-0 z-0 w-full h-full transition-transform duration-200 transform bg-gray-700 rounded-lg shadow-lg"></div>
-              <div className="relative z-10 p-6 transition-transform duration-200 transform bg-gray-800 rounded-lg shadow-lg sm:p-6 group-hover:-translate-y-1 group-hover:translate-x-1 group-hover:shadow-2xl">
-                <h3 className="text-xl font-medium text-green-400 sm:text-md">
-                  Starter
-                </h3>
-                <p className="mt-2 text-3xl font-bold text-gray-300 xl:text-4xl">
-                  <span className="text-gray-300">$0</span>
-                  <span className="ml-2 text-sm text-gray-300">(free)</span>
-                </p>
-                <ul className="mt-4 text-xs text-left">
-                  <li className="mt-2 text-gray-300">
-                    • Access to 10 essential questions
-                  </li>
-                  <li className="mt-2 text-gray-300">
-                    • Basic feedback to get you started
-                  </li>
-                  <li className="mt-2 text-gray-300">
-                    • Limited hints to guide your learning
-                  </li>
-                  <li className="mt-2 text-gray-300">
-                    • No exclusive resources or advanced content
-                  </li>
-                </ul>
-                <ScrollLink
-                  to="intro"
-                  smooth={true}
-                  duration={500}
-                  className="inline-block px-4 py-2 mt-6 font-bold text-black transition duration-300 bg-green-400 rounded-xl hover:bg-green-500 hover:text-white hover:shadow-lg group/link hover:cursor-pointer"
-                >
-                  Get Started
-                  <FaArrowRightLong className="inline-block ml-2 group-hover:scale-110 group-hover:-rotate-45 group-hover/link:translate-x-1 group-hover/link:-translate-y-1 group-hover/link:rotate-45 arrow-animation" />
-                </ScrollLink>
-              </div>
-            </div>
+          <ul className="mt-4 text-sm text-left">
+            <li className="mt-2 text-gray-300">
+              • Access to 10 essential questions
+            </li>
+            <li className="mt-2 text-gray-300">
+              • Basic feedback to get you started
+            </li>
+            <li className="mt-2 text-gray-300">
+              • Limited hints to guide your learning
+            </li>
+            <li className="mt-2 text-gray-300">
+              • No exclusive resources or advanced content
+            </li>
+          </ul>
+          <ScrollLink
+            to="intro"
+            smooth={true}
+            duration={500}
+            className="inline-block px-4 py-2 mt-6 font-bold text-black transition duration-300 bg-green-400 rounded-xl hover:bg-green-500 hover:text-white hover:shadow-lg"
+          >
+            Get Started
+            <FaArrowRightLong className="inline-block ml-2 transition-transform duration-300 transform -rotate-45 hover:translate-x-1 hover:-translate-y-1 hover:rotate-45" />
+          </ScrollLink>
+        </div>
 
-            <div className="relative group">
-              <div className="absolute inset-0 z-0 w-full h-full transition-transform duration-200 transform bg-gray-700 rounded-lg shadow-lg"></div>
-              <div className="relative z-10 p-6 transition-transform duration-200 transform bg-gray-800 rounded-lg shadow-lg sm:p-6 hover:-translate-y-1 hover:translate-x-1 group-hover:shadow-2xl">
-                <h3 className="text-xl font-medium text-blue-400 sm:text-md">
-                  Pro (Monthly)
-                </h3>
-                <p className="mt-2 text-3xl font-bold text-gray-300 xl:text-4xl">
-                  <span className="text-gray-300">$4.99</span>
-                  <span className="ml-2 text-sm text-gray-300">per month</span>
-                </p>
-                <ul className="mt-4 text-xs text-left">
-                  <li className="mt-2 text-gray-300">
-                    • Unlimited access to all questions
-                  </li>
-                  <li className="mt-2 text-gray-300">
-                    • Detailed feedback on every question
-                  </li>
-                  <li className="mt-2 text-gray-300">• Unlimited hints</li>
-                  <li className="mt-2 text-gray-300">
-                    • Exclusive resources to boost your learning
-                  </li>
-                </ul>
-                <ScrollLink
-                  to="intro"
-                  smooth={true}
-                  duration={500}
-                  className="inline-block px-4 py-2 mt-6 font-bold text-black transition duration-300 bg-blue-400 hover:cursor-pointer rounded-xl hover:bg-blue-500 hover:text-white hover:shadow-lg group/link"
-                >
-                  Get Started
-                  <FaArrowRightLong className="inline-block ml-2 group-hover:scale-110 group-hover:-rotate-45 group-hover/link:translate-x-1 group-hover/link:-translate-y-1 group-hover/link:rotate-45 arrow-animation" />
-                </ScrollLink>
+        <div className="relative z-10 w-full max-w-sm p-6 mt-8 transition-all duration-200 border-2 border-gray-500 bg-gray-950 rounded-xl hover:border-gray-300">
+          <h3 className="text-xl font-medium text-blue-400 sm:text-md">
+            Pro (Monthly)
+          </h3>
+          <p className="mt-2 text-3xl font-bold text-gray-300 xl:text-4xl">
+            <span className="text-gray-300">$4.99</span>
+            <span className="ml-2 text-sm text-gray-300">per month</span>
+          </p>
+          <ul className="mt-4 text-sm text-left">
+            <li className="mt-2 text-gray-300">
+              • Unlimited access to all questions
+            </li>
+            <li className="mt-2 text-gray-300">
+              • Detailed feedback on every question
+            </li>
+            <li className="mt-2 text-gray-300">• Unlimited hints</li>
+            <li className="mt-2 text-gray-300">
+              • Exclusive resources to boost your learning
+            </li>
+          </ul>
+          <ScrollLink
+            to="intro"
+            smooth={true}
+            duration={500}
+            className="inline-block px-4 py-2 mt-6 font-bold text-black transition duration-300 bg-blue-400 rounded-xl hover:bg-blue-500 hover:text-white hover:shadow-lg"
+          >
+            Get Started
+            <FaArrowRightLong className="inline-block ml-2 transition-transform duration-300 transform -rotate-45 hover:translate-x-1 hover:-translate-y-1 hover:rotate-45" />
+          </ScrollLink>
+        </div>
 
-                {/* <button
-                  onClick={() =>
-                    handleCheckout("price_1PfQac2LYK3gCcnX2jnYp8du")
-                  }
-                  className="inline-block px-4 py-2 mt-6 font-bold text-black transition duration-300 bg-blue-400 rounded-xl hover:bg-blue-500 hover:text-white hover:shadow-lg group/link"
-                >
-                  Get Started
-                  <FaArrowRightLong className="inline-block ml-2 group-hover:scale-110 group-hover:-rotate-45 group-hover/link:translate-x-1 group-hover/link:-translate-y-1 group-hover/link:rotate-45 arrow-animation" />
-                </button> */}
-              </div>
-            </div>
-
-            <div className="relative group">
-              <div className="absolute inset-0 z-0 w-full h-full transition-transform duration-200 transform bg-gray-700 rounded-lg shadow-lg"></div>
-              <div className="relative z-10 p-6 transition-transform duration-200 transform bg-gray-800 rounded-lg shadow-lg sm:p-6 hover:-translate-y-1 hover:translate-x-1 group-hover:shadow-2xl">
-                <h3 className="text-xl font-medium text-purple-400 sm:text-md">
-                  Pro (Annual)
-                </h3>
-                <p className="mt-2 text-3xl font-bold text-gray-300 xl:text-4xl">
-                  <span className="text-gray-300">$49.99</span>
-                  <span className="ml-2 text-sm text-gray-300">per year</span>
-                </p>
-                <ul className="mt-4 text-xs text-left">
-                  <li className="mt-2 text-gray-300">
-                    • Unlimited access to all questions
-                  </li>
-                  <li className="mt-2 text-gray-300">
-                    • Detailed feedback and unlimited hints
-                  </li>
-                  <li className="mt-2 text-gray-300">
-                    • Exclusive resources to boost your learning
-                  </li>
-                  <li className="mt-2 text-gray-300">
-                    • Save{" "}
-                    <span className="font-bold text-purple-400">17%</span>{" "}
-                    compared to monthly plan
-                  </li>
-                </ul>
-                <ScrollLink
-                  to="intro"
-                  smooth={true}
-                  duration={500}
-                  className="inline-block px-4 py-2 mt-6 font-bold text-black transition duration-300 bg-purple-400 hover:cursor-pointer rounded-xl hover:bg-purple-500 hover:text-white hover:shadow-lg group/link"
-                >
-                  Get Started
-                  <FaArrowRightLong className="inline-block ml-2 group-hover:scale-110 group-hover:-rotate-45 group-hover/link:translate-x-1 group-hover/link:-translate-y-1 group-hover/link:rotate-45 arrow-animation" />
-                </ScrollLink>
-                {/* <button
-                  onClick={() =>
-                    handleCheckout("price_1PfmdZ2LYK3gCcnX8NT8jDok")
-                  }
-                  className="inline-block px-4 py-2 mt-6 font-bold text-black transition duration-300 bg-purple-400 rounded-xl hover:bg-purple-500 hover:text-white hover:shadow-lg group/link"
-                >
-                  Get Started
-                  <FaArrowRightLong className="inline-block ml-2 group-hover:scale-110 group-hover:-rotate-45 group-hover/link:translate-x-1 group-hover/link:-translate-y-1 group-hover/link:rotate-45 arrow-animation" />
-                </button> */}
-              </div>
-            </div>
-          </div>
+        <div className="relative z-10 w-full max-w-sm p-6 mt-8 transition-all duration-200 border-2 border-gray-500 bg-gray-950 rounded-xl hover:border-gray-300">
+          <h3 className="text-xl font-medium text-purple-400 sm:text-md">
+            Pro (Annual)
+          </h3>
+          <p className="mt-2 text-3xl font-bold text-gray-300 xl:text-4xl">
+            <span className="text-gray-300">$49.99</span>
+            <span className="ml-2 text-sm text-gray-300">per year</span>
+          </p>
+          <ul className="mt-4 text-sm text-left">
+            <li className="mt-2 text-gray-300">
+              • Unlimited access to all questions
+            </li>
+            <li className="mt-2 text-gray-300">
+              • Detailed feedback and unlimited hints
+            </li>
+            <li className="mt-2 text-gray-300">
+              • Exclusive resources to boost your learning
+            </li>
+            <li className="mt-2 text-gray-300">
+              • Save <span className="font-bold text-purple-400">17%</span>{" "}
+              compared to monthly plan
+            </li>
+          </ul>
+          <ScrollLink
+            to="intro"
+            smooth={true}
+            duration={500}
+            className="inline-block px-4 py-2 mt-6 font-bold text-black transition duration-300 bg-purple-400 rounded-xl hover:bg-purple-500 hover:text-white hover:shadow-lg"
+          >
+            Get Started
+            <FaArrowRightLong className="inline-block ml-2 transition-transform duration-300 transform -rotate-45 hover:translate-x-1 hover:-translate-y-1 hover:rotate-45" />
+          </ScrollLink>
         </div>
       </div>
     </div>
